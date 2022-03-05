95553a5f65ad5f02817d44682eb4b6ffc4d771f2
Inoela <elena_inostroza@yahoo.com>
2022-03-05 21:07:58 +0100

    added pictures

--- app/src/main/java/api/PexelApi.kt
+++ app/src/main/java/api/PexelApi.kt
@@ -7,9 +7,9 @@ import retrofit2.http.Query
 
 
 interface PexelApi {
-
+/*
         @Headers("Authorization: 563492ad6f91700001000001bd0e748994414fcb845b9ab40da21faf")
         @GET("v1/collections")
         fun getPexels(): Single<PexelsWrapper>
-
+*/
 }
