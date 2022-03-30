cad86f0eec814bca03283f8b77f5f61444f0a912
Inostroza <elena.inostroza@nokia.com>
2022-03-30 22:52:36 +0200

    finished diary works

--- app/src/main/java/com/example/myroutine/SportFragment.kt
+++ app/src/main/java/com/example/myroutine/SportFragment.kt
@@ -1,26 +1,16 @@
 package com.example.myroutine
 import adapter.ExerciseAdapter
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.NavOptions
-import androidx.navigation.Navigation
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.navOptions
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import api.ExerciseDto
 import api.ExerciseRepo
 import com.example.myroutine.databinding.FragmentSportBinding
 import data.Exercise
-import data.toExercise
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 
 class SportFragment : Fragment() {
